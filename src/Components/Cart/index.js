f3f4b2f813fb57b6851df3c04f099d61e896017f
UppariAkshay <uppari.akshay@gmail.com>
2025-01-27 00:20:11 +0530

    implemented order placement and storing orders in database

--- src/Components/Cart/index.js
+++ src/Components/Cart/index.js
@@ -1,22 +1,58 @@
 import './index.css'
 import NavBar from '../Navbar'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import CartContext from '../../Context'
 
 function Cart()
 {
+    const [orderPlaced, setOrderPlaced] = useState(false)
     const {cartItems, setCartItems} = useContext(CartContext)
     console.log(cartItems)
 
+    const placeOrder = async () => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({orderItems: cartItems})
+        }
+
+        const response = await fetch('http://localhost:5000/place-order', options)
+        const responseDaata = await response.json()
+
+        if (response.ok)
+        {
+            console.log(responseDaata, 'order items in frontend')
+        }
+
+        setOrderPlaced(true)
+    }
+
+    const displayOrderPlaced = () => {
+        return <div>
+            <img className='greenTick' src='https://res.cloudinary.com/diazj64up/image/upload/v1737911303/greentick_isdip9.png' alt='green tick'/>
+            <h1>Order Placed Successfully</h1>
+        </div>
+    }
+
+    const displayCartItems = () => {
+        return <div>
+                    <h1>Items In Cart</h1>
+                    <ul className='cartItemsUL'>
+                        {cartItems && cartItems.map(eachProduct => <li className='productCard'>
+                            <p>{eachProduct.productName}</p>
+                            <p>{eachProduct.category}</p>
+                        </li> )}
+                    </ul>
+                    <button disabled={cartItems.length===0} onClick={placeOrder}>Place Order</button>
+               </div>
+    }
+
     return <div>
         <NavBar userType='USER' />
-        <h1>Items In Cart</h1>
-        <ul className='cartItemsUL'>
-            {cartItems && cartItems.map(eachProduct => <li className='productCard'>
-                <p>{eachProduct.productName}</p>
-                <p>{eachProduct.category}</p>
-            </li> )}
-        </ul>
+        {orderPlaced ? displayOrderPlaced() : displayCartItems()}
+        
     </div>
 }
 
